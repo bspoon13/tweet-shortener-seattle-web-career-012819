bd1c257d6b12bc74602746de9bd7292872d4ea49
Billy Witherspoon <wwither@gmail.com>
2019-01-25 04:37:06 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def dictionary(long_word)
+short_word
+  case (long_word)
+  when "hello"
+    short_word = "hi"
+  when "to"
+    short_word = "2"
+  when "two"
+    short_word = "2"
+  when "too"
+    short_word = "2"  
+  when "four"
+    short_word = "4"
+  when "for"
+    short_word = "4"
+  when "be"
+    short_word = "b"
+  when "you"
+    short_word = "u"
+  when "at"
+    short_word = "@"
+  when "and"
+    short_word = "&"
+short_word
+end
